98f486d37d557601d67447a42cafa0a56f52bdf1
unknown <porcelalanu@gmail.com>
2023-02-05 16:26:40 -0300

    aaa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save toaaaaa.
+          Edit <code>src/App.js</code> Ahora sí sé subir esta porqueríaaa.
         </p>
         <a
           className="App-link"
