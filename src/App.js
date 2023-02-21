fd04a199130608ce9e10bf6c3b0b884f16125b88
unknown <porcelalanu@gmail.com>
2023-02-21 20:06:20 -0300

    agregué un 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Ahora sí sé subir esta porqueríaaa.
+          Edit <code>src/App.js</code> Ahora sí sé subir esta porqueríaaa 22.
         </p>
         <a
           className="App-link"
