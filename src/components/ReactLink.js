1b95c19649105f78317ef3b46a7d1db854a3c1bb
unknown <porcelalanu@gmail.com>
2023-02-23 08:10:26 -0300

    componentes 23-2-23

---
+++ src/components/ReactLink.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ReactLink() {
+
+return <a
+className="App-link"
+href="https://parquepodcast.com"
+target="_blank"
+rel="noopener noreferrer"
+>
+Learn React
+</a>
+}
+
+export default ReactLink; 
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Title() {
+
+return <p> Edit pelotudoo</p>
+}
+
+export default Title; 
